29446bd049795a47738c14a018d278495f966c68
June Ha <june.ha@shopify.com>
2023-08-04 11:57:09 -0700

    Initial Commit

---
+++ playground-swiftui/playground_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  playground_swiftuiApp.swift
+//  playground-swiftui
+//
+//  Created by June Ha on 2023-08-04.
+//
+
+import SwiftUI
+
+@main
+struct playground_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
