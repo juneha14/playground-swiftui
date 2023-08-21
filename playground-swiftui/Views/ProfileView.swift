762a938b0de959b18dcdc7de214018377605a0fa
June Ha <june.ha@shopify.com>
2023-08-21 11:22:59 -0700

    Accordion

--- playground-swiftui/Views/ProfileView.swift
+++ playground-swiftui/Views/ProfileView.swift
@@ -11,6 +11,13 @@ struct ProfileView: View {
     private struct Constants {
         static let backgroundHeight = 170.0
         static let backgroundCoordinateSpace = "background-coordinate-space"
+        
+        static let faq: [FAQView.FAQ] = [
+            .init(question: "How many team members can I invite?", answer: "There is no limit!"),
+            .init(question: "What is the maximum file upload size?", answer: "No more than 2GB. All files in your account must fit your allotted storage space."),
+            .init(question: "How do I reset my password?", answer: "Go to Settings > Password"),
+            .init(question: "How do I cancel my subscription?", answer: "Text STOP to 101234"),
+        ]
     }
     
     @State private var backgroundOffsetY: CGFloat = 0
@@ -51,12 +58,12 @@ struct ProfileView: View {
                         .foregroundColor(.gray)
                         .padding(.top, 1)
                 }
-
+                
                 Divider()
                     .frame(height: 1)
                     .padding(.vertical, 10)
                     .padding(.horizontal)
-
+                
                 // Stats
                 HStack(spacing: 50) {
                     StatView(count: 80, title: "Followers")
@@ -66,6 +73,15 @@ struct ProfileView: View {
             }
             .padding(.bottom)
             .padding(.top, -50)
+            
+            
+            // Accordion
+            VStack {
+                ForEach(Constants.faq) { faq in
+                    FAQView(faq: faq)
+                }
+            }
+            .padding()
         }
         .ignoresSafeArea()
     }
@@ -87,11 +103,52 @@ struct StatView: View {
     }
 }
 
+struct FAQView: View {
+    struct FAQ: Identifiable {
+        let id = UUID()
+        let question: String
+        let answer: String
+    }
+    
+    let faq: FAQ
+    
+    @State private var isExpanded = false
+    
+    private struct Constants {
+        static let chevronColor = Color(red: 244/255, green: 124/255, blue: 87/255)
+    }
+    
+    var body: some View {
+        VStack {
+            DisclosureGroup(isExpanded: $isExpanded) {
+                Text(faq.answer)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 8)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            } label: {
+                Text(faq.question)
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                    .bold(isExpanded)
+            }
+            .accentColor(Constants.chevronColor)
+            .padding(.vertical, 4)
+            
+            Divider()
+                .frame(height: 1)
+        }
+    }
+}
+
+
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
     }
 }
+
+// SizeModifiers
 
 struct SizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize { .zero }
