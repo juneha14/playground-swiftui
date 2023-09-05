56e8ad6a234335261bc4625c9130ce145d102acb
June Ha <june.ha@shopify.com>
2023-09-05 11:36:59 -0700

    Product details UI scaffold

---
+++ playground-swiftui/Views/ProductDetailCart.swift
@@ -0,0 +1,193 @@
+//
+//  ProductDetailCart.swift
+//  playground-swiftui
+//
+//  Created by June Ha on 2023-09-01.
+//
+
+import SwiftUI
+
+struct ProductDetailCart: View {
+    var body: some View {
+        TabView {
+            ProductDetails()
+                .tabItem {
+                    Label("Product", systemImage: "house")
+                }
+            Cart()
+                .tabItem {
+                    Label("Cart", systemImage: "cart")
+                }
+        }
+    }
+}
+
+struct ProductDetails: View {
+    private let productImageIds = [
+        "shoe1",
+        "shoe2",
+        "shoe3",
+        "shoe4"
+    ]
+    
+    @State private var quantity = 0
+    @State private var selectedPageIndex: Array.Index = 0
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            // In <iOS 17, we need to use TabView which doubles as a UIPageViewController
+            // For iOS 17, we can use a ScrollView to enable pagination
+            ZStack {
+                TabView(selection: $selectedPageIndex) {
+                    ForEach(productImageIds, id: \.self) { imageId in
+                        if let index = productImageIds.firstIndex(of: imageId) {
+                            Image(imageId)
+                                .resizable()
+                                .scaledToFill()
+                                .tag(index)
+                        }
+                    }
+                }
+                .frame(width: 395, height: 300)
+                .tabViewStyle(.page)
+                .animation(.easeIn, value: selectedPageIndex) // Slide animation when programmatically changing index with button
+                
+                HStack {
+                    Button {
+                        let newIndex = selectedPageIndex - 1
+                        selectedPageIndex = max(0, newIndex)
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .modifier(PaginationButtonModifier())
+                            .padding(.leading)
+                    }
+                    Spacer()
+                    Button {
+                        let newIndex = selectedPageIndex + 1
+                        selectedPageIndex = min(3, newIndex)
+                    } label: {
+                        Image(systemName: "chevron.right")
+                            .modifier(PaginationButtonModifier())
+                            .padding(.trailing)
+                    }
+                }
+            }
+            
+            Group {
+                VStack(alignment: .leading) {
+                    Text("Sneaker Company")
+                        .font(.headline)
+                        .foregroundColor(Theme.orange)
+                        .textCase(.uppercase)
+                    
+                    Text("Fall Limited Edition Sneakers")
+                        .font(.title)
+                        .bold()
+                        .padding(.vertical, 1)
+                    
+                    Text("These low profile sneakers are perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                .padding(.top)
+                
+                LabeledContent {
+                    Text("$250.00")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .strikethrough(true, color: .secondary)
+                } label: {
+                    HStack {
+                        Text("$125.00")
+                            .font(.title)
+                            .bold()
+                            .padding(.trailing, 4)
+                        Text("50%")
+                            .font(.subheadline)
+                            .foregroundColor(Theme.orange)
+                            .bold()
+                            .padding(6)
+                            .background(Theme.paleOrange)
+                            .cornerRadius(8)
+                    }
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button {
+                        let newQuantity = quantity - 1
+                        quantity = max(0, newQuantity)
+                    } label: {
+                        Image(systemName: "minus").foregroundColor(Theme.orange)
+                    }
+
+                    Spacer()
+                    Text(String(quantity))
+                    Spacer()
+                    
+                    Button {
+                        quantity += 1
+                    } label: {
+                        Image(systemName: "plus").foregroundColor(Theme.orange)
+                    }
+                }
+                .bold()
+                .padding()
+                .background(Theme.lightGray)
+                .cornerRadius(8)
+                
+                Button {
+                    //
+                } label: {
+                    Label("Add to cart", systemImage: "cart")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Theme.orange)
+                        .cornerRadius(8)
+                }
+                .buttonStyle(.plain)
+                .padding(.vertical)
+            }
+            .padding(.horizontal)
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct Cart: View {
+    var body: some View {
+        Text("Cart")
+    }
+}
+
+struct ProductDetailCart_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailCart()
+    }
+}
+
+// MARK: - Modifiers
+
+fileprivate struct PaginationButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 5, height: 5)
+            .bold()
+            .foregroundColor(.black)
+            .padding()
+            .background(.white)
+            .clipShape(Circle())
+    }
+}
+
+
+// MARK: - Theme
+
+fileprivate struct Theme {
+    static let orange = Color.orange
+    static let paleOrange = Color(red: 255/255, green: 237/255, blue: 224/255)
+    static let lightGray = Color(red: 247/255, green: 248/255, blue: 253/255)
+}
